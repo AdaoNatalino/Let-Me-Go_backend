8713cf02be11d2387f47051caadb1370ed6d862f
Adao Natalino <adaojosen@hotmail.com>
2020-07-16 19:12:39 +0100

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
    
     def create
       @user = User.create(user_params)
-    #   byebug
       if @user.valid?
         @token = encode_token( { user_id: @user.id } )
         render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
