e0ca2277144ef25d5f54a2c5972afce6d3a65cdf
Adao Natalino <adaojosen@hotmail.com>
2020-07-19 19:57:57 +0100

    item created

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,19 @@
 class ItemsController < ApplicationController
-    skip_before_action :authorized, only: [:index]
+    # skip_before_action :authorized, only: [:index]
 
     def index
         @items = Item.all
         render json: @items, each_serializer: ItemSerializer
+    end
+
+    def create
+        @item = Item.create(item_params)
+        # byebug
+        if @item.valid?
+          render json: { item: ItemSerializer.new(@item) }, status: :created
+        else
+          render json: { error: @item.errors.full_messages }, status: :not_acceptable
+        end
     end
 
     def category
@@ -15,5 +25,10 @@ class ItemsController < ApplicationController
         # render json: items
     end
 
+    private
+   
+    def item_params
+      params.require(:item).permit(:name, :description, :condition, :category_id, :image, :user_id)
+    end
    
 end
