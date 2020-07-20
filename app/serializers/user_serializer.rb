6a649464d2fe7d3fb6e91abea0f6b3d44e3f1db2
Adao Natalino <adaojosen@hotmail.com>
2020-07-20 19:30:56 +0100

    fim

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :name, :username, :city, :email, :avatar, :bio, :stars, :id
+  attributes :name, :username, :city, :email, :avatar, :bio, :stars, :id, :trades, :items
 end
