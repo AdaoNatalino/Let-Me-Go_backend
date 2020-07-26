32170c9ad06ce392d0bb370a6c9cca78f04c932e
Adao Natalino <adaojosen@hotmail.com>
2020-07-26 15:30:55 +0100

    Revert "seeds"
    This reverts commit 3fb5b6096443293a222223b61b22304c6cffcd13.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+    # skip_before_action :authorized, only: [:index]
 
     def index
       @items = Item.all
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     has_many :items, dependent: :destroy
     has_secure_password
 
+    #validations:
     validates :name, :username, :email, :city, :bio, presence: true
     validates :name, length: { in: 2..100 }
     validates :password, length: { in: 5..20 }
