8c29a363cd20ec41fd27d96b0a3e53f7f28c56a3
Adao Natalino <adaojosen@hotmail.com>
2020-07-27 19:15:57 +0100

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://let-me-go.netlify.app/'
+    origins 'https://let-me-go.netlify.app'
 
     resource '*',
       headers: :any,
