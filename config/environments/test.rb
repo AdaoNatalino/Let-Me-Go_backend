df518bed80d308a08107d608acb24221dc38c5dc
Adao Natalino <adaojosen@hotmail.com>
2020-08-03 21:31:06 +0100

    comments

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,7 +1,4 @@
-# The test environment is used exclusively to run your application's
-# test suite. You never need to work with it otherwise. Remember that
-# your test database is "scratch space" for the test suite and is wiped
-# and recreated between test runs. Don't rely on the data there!
+
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -9,9 +6,7 @@ Rails.application.configure do
   config.cache_classes = false
   config.action_view.cache_template_loading = true
 
-  # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
+
   config.eager_load = false
 
   # Configure public file server for tests with Cache-Control for performance.
